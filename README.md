27ccc33a70a5d5ba0deb68f158f68388a8b3e884
duncan_mearns <mearns@neuro.mpg.de>
2021-05-31 18:16:34 +0200

    Tests, license, setup, readme

---
+++ README.md
@@ -0,0 +1,4 @@
+Tail tracker
+============
+
+Head-embedded tail tracking package. Includes PyQt GUI for initializing tracking parameters.
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='tailtracker',
+    version='0.0.1',
+    packages=['tailtracker', 'tailtracker.gui'],
+    url='',
+    license='GPLv3',
+    author='mearns',
+    author_email='mearns@neuro.mpg.de',
+    description='Code for head-embedded tail tracking.'
+)
